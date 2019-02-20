8e4fe90d7791640aa40569efaa16e69f1b1f4f41
Andy Ren <aren20113@gmail.com>
2019-02-20 16:30:36 -0500

    updated main.js

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 "use strict";
 
-let userInput = prompt("Input a function", "x");
+let userInput = prompt("Input a function", "1x^1+1");
 let polynomialArray = [];
 
 function arrayGenerator(polynomial) {
     polynomial.forEach((monomial) => {
       let polynomialProps = monomial.split('^');
       polynomialArray.push({
-          coefficient: parseFloat(monomial[0]),
-          power: function(monomial) {
-            if(monomial[1] != null) {
-              return parseInt(monomial[1]);
-            } else {
-              return 0;
-            }
-          }
+          coefficient: parseFloat(polynomialProps[0]),
+          power: polynomialProps[1] != null ? parseInt(polynomialProps[1]) : 0
       });
     });
     console.log(polynomialArray);
 }
 
-arrayGenerator(userInput.match(/[+-]?\d+x\^\d+|[+-\s]\d[+-\s]/g));
+//pass in user input with a regex filter for [+/-][number]x^[number]
+arrayGenerator(userInput.match(/[+-]?\d+x\^\d+|[+-\s]\d+[+-\s]/g));
+
+// proto regex /[+-]?\d+x\^\d+|[+-\s]\d+[+-\s]|[+-]x\^\d|[+-]?x/g
+// orig regex /[+-]?\d+x\^\d+|[+-\s]\d[+-\s]/g
+
+/* function(polynomialProps) {
+  if(polynomialProps[1] != null) {
+    return parseInt(PolynomialProps[1]);
+  } else {
+    return 0;
+  } */
+
+
+  /*
+  * Functions to be written
+  * add
+  *
+  *
+  *
+  *
+  *
+  */
