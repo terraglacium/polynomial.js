a042d466d43f9dc07ce4657f7b7713077cacb712
Andy Ren <aren20113@gmail.com>
2019-02-27 17:30:48 -0500

    updated more scripts

--- functions/factor.js
+++ functions/factor.js
@@ -1,1 +1,11 @@
 //recursive function
+"use strict";
+
+function factor(polyArray) {
+  let pSlashq = polyArray[0].coefficient / polyArray[polyArray.length-1].coefficient;
+
+
+
+}
+
+module.exports = factor;
